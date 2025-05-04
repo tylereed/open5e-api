05a839a661c81c00e50112098d8653ca61b0b1aa
Tyler Reed <tylereed@gmail.com>
2025-05-03 20:29:53 -0600

    Updated parsing code to cleanup action name, and pull out uses_type and uses_param

--- scripts/data_manipulation/monsterattacks.py
+++ scripts/data_manipulation/monsterattacks.py
@@ -1,5 +1,7 @@
 from django.template.defaultfilters import slugify
 
+import re
+
 from api.models import Monster as v1_model
 from api_v2.models import Creature as v2_model, CreatureAction as v2_creatureaction, CreatureActionAttack as v2_creatureattack
 from .antlr4.parsers import parseAttack, parseDice, buildCsvHeader
@@ -24,15 +26,36 @@ def add_v2_attack(v1_monster: v1_model, v2_creature: v2_model):
         order=0
         for v1_action in actions:
             # try:
-                v2_action_key = get_v2_action_key(v2_creature.key, v1_action['name'])
+                v1_action_name = v1_action['name']
+                v1_action_cleaned_name = re.sub(' \(.*\)', '', v1_action_name)
+                re_v1_additional_info = re.search('\((.*)\)', v1_action_name)
+                v1_additional_info = re_v1_additional_info.group(0) if re_v1_additional_info else None
+                
+                parsed_uses_type = None
+                parsed_uses_param = None
+                
+                if v1_additional_info:
+                    if re.search('Recharge after a Short or Long rest', v1_additional_info, flags=re.IGNORECASE):
+                        parsed_uses_type = 'RECHARGE_AFTER_REST'
+                        parsed_uses_param = None
+                    elif recharge := re.search('Recharge ((\d+)-)?(\d+)', v1_additional_info):
+                            parsed_uses_type = 'RECHARGE_ON_ROLL'
+                            parsed_uses_param = int(recharge.group(2) or recharge.group(3))
+                    elif per_day := re.search('(\d+)/Day', v1_additional_info, flags=re.IGNORECASE):
+                            parsed_uses_type = 'PER_DAY'
+                            parsed_uses_param = int(per_day.group(1))
+
+                v2_action_key = get_v2_action_key(v2_creature.key, v1_action_cleaned_name)
+                
+                #print(v1_action_name, v1_action_cleaned_name, v1_additional_info, parsed_uses_type, parsed_uses_param, sep=",")
 
                 # TODO parse out form from name and set, also recharge
-                v2_action = v2_creatureaction(name=v1_action['name'],
+                v2_action = v2_creatureaction(name=v1_action_cleaned_name,
                     desc=v1_action['desc'],
                     key = v2_action_key,
                     parent=v2_creature,
-                    uses_type=None,
-                    uses_param=None,
+                    uses_type=parsed_uses_type,
+                    uses_param=parsed_uses_param,
                     action_type='ACTION',
                     form_condition=None,
                     legendary_cost=None,
